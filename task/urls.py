96106b7518a3679d741e69ba3aed3fc4d9e0c47c
Vladyslav Demydenko <bublikyo3@gmail.com>
2023-04-03 11:19:40 +0200

    Add url, view and template for Tag list page

--- task/urls.py
+++ task/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from task.views import TaskListView
+from task.views import TaskListView, TagsListView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
+    path("tags/", TagsListView.as_view(), name="tag-list")
 ]
 
 app_name = "task"
--- task/views.py
+++ task/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 
-from task.models import Task
+from task.models import Task, Tag
 
 
 class TaskListView(generic.ListView):
@@ -11,3 +11,7 @@ class TaskListView(generic.ListView):
     def get_queryset(self):
         self.queryset = Task.objects.order_by("task_is_done", "-created")
         return self.queryset
+
+
+class TagsListView(generic.ListView):
+    model = Tag
